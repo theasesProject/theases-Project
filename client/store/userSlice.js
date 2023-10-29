1fd6525720cd1150e19d8815c31ca73ebba12d5c
Alythç <77921590+Jihed-Garroum@users.noreply.github.com>
2023-10-29 17:39:50 +0100

    ll

--- client/store/userSlice.js
+++ client/store/userSlice.js
@@ -26,7 +26,8 @@ const fetchUser = createAsyncThunk("user/fetchUser", async (token) => {
 export const SignUpClick = createAsyncThunk("user/SignUp", async (inputForm,) => {
   try {
     console.log(inputForm);
-    const task = await axios.post(`http://${process.env.EXPO_PUBLIC_SERVER_IP}:5000/api/users/SignUpUser`, inputForm)
+    const task = await axios.post(`http://192.168.1.13:5000/api/users/SignUpUser`, inputForm)
+
     return task.data
   } catch (er) {
     console.error(JSON.stringify(er));

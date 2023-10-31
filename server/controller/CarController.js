6ad56af0215e6c88c1630204d5d15b6f7dd0a28d
LouhichiMedSelim <lmedselim@gmail.com>
2023-10-31 01:13:02 +0100

    sss

--- server/controller/CarController.js
+++ server/controller/CarController.js
@@ -1,129 +1,102 @@
+const { db } = require("../models/index");
+const { Sequelize, Op } = require("sequelize");
 
-const {db}=require("../models/index")
-const { Sequelize, Op } = require('sequelize')
+module.exports = {
+  getAllCars: async function (req, res) {
+    try {
+      const allCars = await db.Car.findAll({
+        include: { model: db.CarMedia, as: "CarMedia" },
+      });
 
-module.exports={
-    getAllCars: async function (req,res){
-        try {
-            const allCars= await db.Car.findAll({
-              include: { model: db.CarMedia, as: 'CarMedia' }
-            })
- 
-            res.status(200).send(allCars)
-        } catch (error) {
-            throw error
-        
-        }
-     },
-  
+      res.status(200).send(allCars);
+    } catch (error) {
+      throw error;
+    }
+  },
 
-     
-     CreateCar:async function (req,res){
-        try{
-          const newCar=await db.Car.create({
-        
-            
-                model: req.body.model,
-                brand: req.body.brand,
-                price: req.body.price,
-                period: req.body.period,
-                status: "available",
-                horsePower: req.body.horse,
-                typeOfFuel: req.body.typeOfFuel,
-                description: req.body.description,
-                warrantyInsurance: req.body.warrantyInsurance,
-                deposit: req.body.deposit,
-                acceptation: "pending",
-                typevehicle:req.body.typevehicle,
-                characteristics:req.body.characteristics
-              
-        
-          })
-        
-          res.status(200).send(newCar)
-        } catch (error) {
-            throw error
-        
-        }
-        
-             },
-             createImage: async function (req,res){
-              try {
-                  const image= await db.CarMedia.create({
-                    media:req.body.media,
-                    CarId:req.body.CarId
-                  })
-       
-                  res.status(200).send(image)
-              } catch (error) {
-                  throw error
-              
-              }
-           },
-           filterCarByBrand: async function (req,res){
-            try{
-              const carByBrand=await db.Car.findAll({
-            
-                where:{brand:req.params.brand}
-            
-              })
-            
-              res.status(200).send(carByBrand)
-            } catch (error) {
-                throw error
-         }
-        } ,
+  CreateCar: async function (req, res) {
+    try {
+      const newCar = await db.Car.create({
+        model: req.body.model,
+        brand: req.body.brand,
+        price: req.body.price,
+        period: req.body.period,
+        status: "available",
+        horsePower: req.body.horse,
+        typeOfFuel: req.body.typeOfFuel,
+        description: req.body.description,
+        warrantyInsurance: req.body.warrantyInsurance,
+        deposit: req.body.deposit,
+        acceptation: "pending",
+        typevehicle: req.body.typevehicle,
+        characteristics: req.body.characteristics,
+      });
 
-        searchCarByModel : async function (req,res){
-          const model = req.query.model;
-          try{
-            const carSearched=await db.Car.findAll({
-          
-              where: {
-                model: {
-                  [Op.like]: `%${model}%`,
-                },
-              },
-            })
-          
-           
-          
-            res.status(200).send(carSearched)
-          } catch (error) {
-              throw error
-       }
-      } ,
-      filtredCar : async function (req,res){
+      res.status(200).send(newCar);
+    } catch (error) {
+      throw error;
+    }
+  },
+  createImage: async function (req, res) {
+    try {
+      const image = await db.CarMedia.create({
+        media: req.body.media,
+        CarId: req.body.CarId,
+      });
 
-  const filters = req.body;
-  const { price, typevehicle, characteristics } = filters;
+      res.status(200).send(image);
+    } catch (error) {
+      throw error;
+    }
+  },
+  filterCarByBrand: async function (req, res) {
+    try {
+      const carByBrand = await db.Car.findAll({
+        where: { brand: req.body.brand },
+      });
 
-  const where = {
-    price: {
-      [Op.between]: price,
-    },
-    typevehicle:typevehicle,
- 
+      res.status(200).send(carByBrand);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  searchCarByModel: async function (req, res) {
+    const model = req.params.model;
+    try {
+      const carSearched = await db.Car.findAll({
+        where: {
+          model: {
+            [Op.like]: `%${model}%`,
+          },
+        },
+      });
+
+      res.status(200).send(carSearched);
+    } catch (error) {
+      throw error;
+    }
+  },
+  filtredCar: async function (req, res) {
+    const filters = req.body;
+    const { price, typevehicle, characteristics } = filters;
+
+    const where = {
+      price: {
+        [Op.between]: price,
+      },
+      typevehicle: typevehicle,
+
       characteristics: characteristics,
-    
-  };
-try {
-  const filtredOne=  await db.Car.findAll({
-    where,
-  })
-  
+    };
+    try {
+      const filtredOne = await db.Car.findAll({
+        where,
+      });
+
       res.json(filtredOne);
-}
-    catch (error) {
-      throw error
-}
-  
-  }}
-
-
-
-
-
-
-
-
+    } catch (error) {
+      throw error;
+    }
+  },
+};

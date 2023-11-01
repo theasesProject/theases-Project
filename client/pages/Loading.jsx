9d72e07f1715a7b53e7b2083969df6303e363ae4
zeineb <zeineb.khemida11@gmail.com>
2023-11-01 21:33:38 +0100

    merge

--- client/pages/Loading.jsx
+++ client/pages/Loading.jsx
@@ -1,53 +1,53 @@
-// import React, { Component } from 'react';
-// import { View, Text, Button, StyleSheet } from 'react-native';
-// import Spinner from 'react-native-loading-spinner-overlay'; // Import the Spinner component
+import React, { Component } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import Spinner from "react-native-loading-spinner-overlay"; // Import the Spinner component
 
-// class LoadingScreen extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       isLoading: false,
-//     };
-//   }
+class LoadingScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    };
+  }
 
-//   // Function to start the loading animation
-//   startLoading = () => {
-//     this.setState({ isLoading: true });
-    
-//     // Simulate loading for 3 seconds (You can replace this with actual API calls)
-//     setTimeout(() => {
-//       this.setState({ isLoading: false });
-//     }, 3000);
-//   }
+  // Function to start the loading animation
+  startLoading = () => {
+    this.setState({ isLoading: true });
 
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.text}>Welcome to Loading Animation Page</Text>
-//         <Button title="Start Loading" onPress={this.startLoading} />
-//         <Spinner        
-//           visible={this.state.isLoading}
-//           textContent={'Loading...'}
-//           textStyle={styles.spinnerText}
-//         />
-//       </View>
-//     );
-//   }
-// }
+    // Simulate loading for 3 seconds (You can replace this with actual API calls)
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   text: {
-//     fontSize: 20,
-//     marginBottom: 20,
-//   },
-//   spinnerText: {
-//     color: '#fff',
-//   },
-// });
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome to Loading Animation Page</Text>
+        <Button title="Start Loading" onPress={this.startLoading} />
+        <Spinner
+          visible={this.state.isLoading}
+          textContent={"Loading..."}
+          textStyle={styles.spinnerText}
+        />
+      </View>
+    );
+  }
+}
 
-// export default LoadingScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  spinnerText: {
+    color: "#fff",
+  },
+});
+
+export default LoadingScreen;

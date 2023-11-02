3ac8aadef29a5cda36a5b7bbf9235cb9932f168d
LouhichiMedSelim <lmedselim@gmail.com>
2023-11-02 09:55:09 +0100

    afteer merge

---
+++ client/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import {
+    SafeAreaView,
+    ScrollView,
+    View,
+    Text,
+    Button,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Pressable,
+  } from "react-native";
+  function NavBar({navigation}){
+    return (
+        <View>
+
+        </View>
+    )
+  }
+  export default NavBar
